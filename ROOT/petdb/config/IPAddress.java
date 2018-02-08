d2412e385efb73b13428622296cb6b57dd133ed3
Lulin Song <Lulin.Song@gmail.com>
2018-02-08 11:12:21 -0800

    Test machine IP to ignore list

--- ROOT/petdb/config/IPAddress.java
+++ ROOT/petdb/config/IPAddress.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 public class IPAddress
 {
     private String[] agent = {"baidu","msnbot","googlebot","spider","yahoo","bjtelecom"};
-    private String[] IPignorelist = {"142.104.247.1","68.180.230.178","136.243.36.92","136.243.36.88"};
+    private String[] IPignorelist = {"142.104.247.1","68.180.230.178","136.243.36.92","136.243.36.88","10.0.2.15"};
     
 	public static String getIpAddr(HttpServletRequest request)
 	{		
